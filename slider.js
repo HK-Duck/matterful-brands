96b386d898598e900dae3e05c0d0801633c5369f
Helena <helen.rudkovski@gmail.com>
2025-11-13 18:16:53 +0300

    Fixed an issue with sliders. The Product Range section is now responsive.

--- slider.js
+++ slider.js
@@ -3,3 +3,10 @@ const range = document.querySelector('.slider__range-js');
 range.addEventListener('input', () => {
     slider.style.setProperty('--value', range.value + '%');
 });
+
+
+const sliderSeven = document.querySelector('.slider-seven');
+const rangeSeven = document.querySelector('.slider__range-js7');
+rangeSeven.addEventListener('input', () => {
+    sliderSeven.style.setProperty('--value', rangeSeven.value + '%');
+});
